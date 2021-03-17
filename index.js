576d06ee57e4c6b260fd8c6f630d7daab2125641
OM Miles <olivia.m.miles@gmail.com>
2021-03-16 18:05:23 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+let shout = string => {
+    return string.toUpperCase()
+} 
+
+let whisper = string => {
+    return string.toLowerCase()
+} 
+
+let logShout = string => {
+    console.log(string.toUpperCase())
+} 
+
+let logWhisper = string => {
+    console.log(string.toLowerCase())
+} 
+
+let sayHiToGrandma = string => {
+    if (string === `hello`){
+        return (`I can't hear you!`);
+    } if (string === `HELLO`) {
+        return (`YES INDEED!`)
+    } if (string === `I love you, Grandma.`) {
+        return (`I love you, too.`)
+    }
+} 
