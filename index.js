1ee2caf679ed42438408cce27874c0512d882a63
OM Miles <olivia.m.miles@gmail.com>
2021-03-27 10:26:32 -0700

    Alternative solution added

--- index.js
+++ index.js
@@ -23,3 +23,13 @@ let sayHiToGrandma = string => {
         return (`I love you, too.`)
     }
 } 
+
+// let sayHiToGrandma = string => {
+//     if (string === `hello`){
+//         return (`I can't hear you!`);
+//     } else if (string === `HELLO`) {
+//         return (`YES INDEED!`)
+//     } else (string === `I love you, Grandma.`) {
+//         return (`I love you, too.`)
+//     }
+// } 
